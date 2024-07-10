3d54e863d56a063262382ad4fb4c6e0c9ed32d55
Daniil Moskalenko <justcr1si@yandex.ru>
2024-07-11 00:47:09 +0300

    Make interface for players

--- game.py
+++ game.py
@@ -1,54 +1,133 @@
+# game.py
+
+import pygame
+
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError, CellOccupiedField
+
+pygame.init()
+
+CELL_SIZE = 100
+BOARD_SIZE = 3
+WIDTH = HEIGHT = CELL_SIZE * BOARD_SIZE
+LINE_WIDTH = 15
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+X_COLOR = (84, 84, 84)
+O_COLOR = (242, 235, 211)
+X_WIDTH = 15
+O_WIDTH = 15
+SPACE = CELL_SIZE // 4
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Крестики-нолики')
+screen.fill(BG_COLOR)
+
+
+def draw_lines():
+    for i in range(1, BOARD_SIZE):
+        pygame.draw.line(
+            screen,
+            LINE_COLOR,
+            (0, i * CELL_SIZE),
+            (WIDTH, i * CELL_SIZE),
+            LINE_WIDTH
+        )
+
+    for i in range(1, BOARD_SIZE):
+        pygame.draw.line(
+            screen,
+            LINE_COLOR,
+            (i * CELL_SIZE, 0),
+            (i * CELL_SIZE, HEIGHT),
+            LINE_WIDTH
+        )
+
+
+def draw_figures(board):
+    for row in range(BOARD_SIZE):
+        for col in range(BOARD_SIZE):
+            if board[row][col] == 'X':
+                pygame.draw.line(
+                    screen,
+                    X_COLOR,
+                    (col * CELL_SIZE + SPACE, row * CELL_SIZE + SPACE),
+                    (
+                        col * CELL_SIZE + CELL_SIZE - SPACE,
+                        row * CELL_SIZE + CELL_SIZE - SPACE
+                    ),
+                    X_WIDTH
+                )
+                pygame.draw.line(
+                    screen,
+                    X_COLOR,
+                    (
+                        col * CELL_SIZE + SPACE,
+                        row * CELL_SIZE + CELL_SIZE - SPACE
+                    ),
+                    (
+                        col * CELL_SIZE + CELL_SIZE - SPACE,
+                        row * CELL_SIZE + SPACE
+                    ),
+                    X_WIDTH
+                )
+            elif board[row][col] == 'O':
+                pygame.draw.circle(
+                    screen,
+                    O_COLOR,
+                    (
+                        col * CELL_SIZE + CELL_SIZE // 2,
+                        row * CELL_SIZE + CELL_SIZE // 2
+                    ),
+                    CELL_SIZE // 2 - SPACE,
+                    O_WIDTH
+                )
+
+
+def save_result(result):
+    # Если нужно явно указать кодировку, добавьте параметр encoding='utf-8'.
+    with open('results.txt', 'a', encoding='utf-8') as f:
+        f.write(result + '\n')
 
 
 def main():
     game = Board()
     current_player = 'X'
     running = True
-    game.display()
+    draw_lines()
+
     while running:
-        while True:
-            print(f'Ход делают {current_player}')
-            try:
-                row = int(input('Введите номер строки: ')) - 1
-                if row < 0 or row >= game.field_size:
-                    raise FieldIndexError
-                column = int(input('Введите номер столбца: ')) - 1
-                if column < 0 or column >= game.field_size:
-                    raise FieldIndexError
-                if game.board[row][column] != ' ':
-                    raise CellOccupiedField
-            except FieldIndexError:
-                print(
-                    'Значение должно быть неотрицательным (не равным 0) и меньше '
-                    f'{game.field_size}.'
-                )
-                print('Пожалуйста, введите значения для строки и столбца заново.')
-                continue
-            except CellOccupiedField:
-                print('Ячейка занята')
-                print('Введите другие координаты')
-                continue
-            except ValueError:
-                print('Буквы вводить нельзя. Только числа.')
-                print('Пожалуйста, введите значения для строки и столбца заново.')
-                continue
-            except Exception as e:
-                print(f'Возникла ошибка: {e}')
-            else:
-                break
 
-        game.make_move(row, column, current_player)
-        game.display()
-        if game.check_win(current_player):
-            print(f'Победили {current_player}!')
-            running = False
-        elif game.is_board_full():
-            print('Ничья!')
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        current_player = 'O' if current_player == 'X' else 'X'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_y = event.pos[0]
+                mouse_x = event.pos[1]
+
+                clicked_row = mouse_x // CELL_SIZE
+                clicked_col = mouse_y // CELL_SIZE
+
+                if game.board[clicked_row][clicked_col] == ' ':
+                    game.make_move(clicked_row, clicked_col, current_player)
+
+                    if game.check_win(current_player):
+                        result = f'Победили {current_player}.'
+                        print(result)
+                        save_result(result)
+                        running = False
+                    elif game.is_board_full():
+                        result = 'Ничья!'
+                        print(result)
+                        save_result(result)
+                        running = False
+
+                    current_player = 'O' if current_player == 'X' else 'X'
+                    draw_figures(game.board)
+
+        pygame.display.update()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
--- gameparts/parts.py
+++ gameparts/parts.py
@@ -10,11 +10,6 @@ class Board:
     def make_move(self, row, col, player):
         self.board[row][col] = player
 
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
-
     def is_board_full(self):
         for i in range(self.field_size):
             for j in range(self.field_size):
